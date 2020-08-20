b6aef67c978e2851b8d64c8c881858945b2e9743
Alexander Whan <whan.alexander@gmail.com>
2020-08-19 20:50:58 -0700

    Required Mongoose in index.js and pulled MongoDB URI into env file

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-// Create an express server, using 2 files
-// index.js at the root of your repository, which will act as the “entry point” to your server.
-
 'use strict';
 
-// should require lib/server.js
 const server = require('./lib/server.js');
-// should require dotenv, reading PORT from your .env file
 require('dotenv').config();
+const mongoose = require('mongoose');
 
-// It should call the .start() method from the server with the PORT set in your environment
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
+
 server.start(process.env.PORT);
