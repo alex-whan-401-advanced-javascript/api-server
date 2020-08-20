674a2291686743904638b264e27d064fb581966a
Alexander Whan <whan.alexander@gmail.com>
2020-08-19 23:54:41 -0700

    Refactored category routes into separate function/route references

--- routes/categories.js
+++ routes/categories.js
@@ -6,27 +6,35 @@ const router = express.Router();
 let db = [];
 
 // Categories Routes
-router.get('/categories', (req, res, next) => {
+router.get('/categories', getCategories);
+
+function getCategories(req, res, next) {
   let count = db.length;
   let results = db;
   res.status(200).json({ count, results });
-});
+}
 
-router.get('/categories/:id', (req, res, next) => {
+router.get('/categories/:id', getOneCategory);
+
+function getOneCategory(req, res, next) {
   let id = req.params.id;
   let record = db.filter(record => record.id === parseInt(id));
   res.status(200).json(record[0]);
-});
+}
 
-router.post('/categories', (req, res, next) => {
+router.post('/categories', postCategories);
+
+function postCategories(req, res, next) {
   let { name } = req.body;
   let record = { name };
   record.id = db.length + 1;
   db.push(record);
   res.status(200).json(record);
-});
+}
 
-router.put('/categories/:id', (req, res, next) => {
+router.put('/categories/:id', putCategories);
+
+function putCategories(req, res, next) {
   let idToUpdate = req.params.id;
   let { name, id } = req.body;
   let updatedRecord = { name, id };
@@ -34,13 +42,15 @@ router.put('/categories/:id', (req, res, next) => {
     record.id === parseInt(idToUpdate) ? updatedRecord : record;
   });
   res.status(200).json(updatedRecord);
-});
+}
 
-router.delete('/categories/:id', (req, res, next) => {
+router.delete('/categories/:id', deleteOneCategory);
+
+function deleteOneCategory(req, res, next) {
   let id = req.params.id;
   db = db.filter(record => record.id !== parseInt(id));
   res.status(200).json({});
-});
+}
 
 module.exports = router;
 
