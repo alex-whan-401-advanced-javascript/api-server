a4d10d65771253c778939e32c791a2c0b36959a2
Alexander Whan <whan.alexander@gmail.com>
2020-08-21 07:39:38 -0700

    Refactored category/product routers into single generic router module

--- routes/api-v1.js
+++ routes/api-v1.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 const categories = require('../lib/models/categories/categories-model');
 const products = require('../lib/models/products/products-model');
-const { model } = require('../lib/models/products/products-schema');
 
 function getModel(req, res, next) {
   let model = req.params.model;
@@ -31,63 +30,35 @@ function getModel(req, res, next) {
 router.param('model', getModel);
 
 // Combined route definitions
+router.post('/api/v1/:model', handlePost);
 router.get('/api/v1/:model', handleGetAll);
-router.get('/api/v1/:model', handleGetOne);
-router.get('/api/v1/:model/:id', handlePost);
+router.get('/api/v1/:model/:id', handleGetOne);
+router.put('/api/v1/:model/:id', handleUpdateOne);
+router.delete('/api/v1/:model/:id', handleDeleteOne);
 
-function handleGetAll(req, res, next) {
-  req.model
-    .get()
-    .then(results => {
-      let count = results.length;
-      res.json({ count, results });
-    })
-    .catch(next);
+async function handlePost(req, res, next) {
+  let newRecord = await req.model.create(req.body);
+  res.status(200).json(newRecord);
 }
 
-function handleGetOne(req, res, next) {
-  let id = req.params.id;
-  req.model
-    .get(id)
-    .then(record => res.json(record))
-    .catch(next);
+async function handleGetAll(req, res, next) {
+  let recordsToGet = await req.model.get();
+  res.status(200).json(recordsToGet);
 }
 
-function handlePost(req, res, next) {
-  req.model
-    .post(req.body)
-    .then(results => res.json(results))
-    .catch(next);
+async function handleGetOne(req, res, next) {
+  let recordToGet = await req.model.get(req.params.id);
+  res.status(200).json(recordToGet);
+}
+
+async function handleUpdateOne(req, res, next) {
+  let recordToUpdate = await req.model.update(req.params.id, req.body);
+  res.status(200).json(recordToUpdate);
+}
+
+async function handleDeleteOne(req, res, next) {
+  await req.model.delete(req.params.id);
+  res.status(200).send(`You have deleted a category with ID ${req.params.id}`);
 }
 
 module.exports = router;
-
-// Create a new generic “api” router module
-
-// In this part of the refactor, we’ll be dynamically loading the model specified in the routes, dynamically, and using that in our route handlers for all CRUD operations. This is done in place of requiring the data model as a normal dependency in our route module.
-
-// Name it something generic like lib/routes/api.js or lib/routes/api-v1.js
-
-// Model this after one of your other working routes
-
-// Setup express params middleware at the top level, to run a function on the “model” parameter
-
-// In this middleware, dynamically require() the data model specified by the model parameter:
-
-// Identify a valid model in the route param
-
-// Finds the module in the file system
-
-// Requires and instantiates it
-
-// Makes that model available to the handler functions so that they can still call, for example, `request.model.create()
-
-// Change your route definitions to recognize dynamic models, and engage this new middleware
-
-// i.e. router.get('/api/v1/:model', handleGetAll)
-
-// In your route handlers, use the model your params middleware (above) identified and loaded
-
-// Ideally, these will be on the request object, so you can simply call the methods like:
-
-// request.model.create( request.body ) instead of categories.create( request.body )
